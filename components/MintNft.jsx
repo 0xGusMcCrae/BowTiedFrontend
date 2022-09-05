9f1b5ed2ed340746563286d011a65a14d4a56fc7
0xGusMcCrae <0xGusMcCrae@protonmail.com>
2022-09-05 19:25:29 -0400

    Updated frontend cleaned up code

--- components/MintNft.jsx
+++ components/MintNft.jsx
@@ -9,8 +9,8 @@ export default function MintNft() {
   const {chainId: chainIdHex, isWeb3Enabled} = useMoralis();
   const provider = ethers.getDefaultProvider();
   const chainId = 5;
-  const mintAddress = "0x1Cd96dD820Aa2b618122BDCcF6C65557a29b9801"
-  //const mintContract = new ethers.Contract(abi,mintAddress,provider);
+  const mintAddress = "0x1Cd96dD820Aa2b618122BDCcF6C65557a29b9801" //should try to get this from constants
+  const mintContract = new ethers.Contract(mintAddress,abi,provider);
   const [totalSupply, setTotalSupply] = useState("0");
   const [supplyRemaining, setSupplyRemaining] = useState("0");
 
@@ -21,54 +21,73 @@ export default function MintNft() {
     params: {},
     msgValue: 0,
   })
+
+  const {runContractFunction: maxSupply} = useWeb3Contract({
+    abi: abi,
+    contractAddress: mintAddress,
+    functionName: "maxSupply",
+    params: {},
+    msgValue: 0,
+  })
+
+  const {runContractFunction: getTokenCount} = useWeb3Contract({
+    abi: abi,
+    contractAddress: mintAddress,
+    functionName: "getTokenCount",
+    params: {},
+    msgValue: 0,
+  })
   
 
-  // async function updateUi() {
-  //   const totalSupplyfromCall = (await mintContract.maxSupply()).toString();
-  //   const supplyRemainingfromCall = (totalSupply - (await getTokenCount())).toString();
-  //   setTotalSupply(totalSupplyfromCall);
-  //   setSupplyRemaining(supplyRemainingfromCall);
-  // }
-  // useEffect(() => {
-  //   if(isWeb3Enabled){
+  // Update UI with new supply remaining each time an NFT is minted
+  async function updateUi() {
+    const totalSupplyfromCall = (await maxSupply()).toString();
+    const supplyRemainingfromCall = (totalSupplyfromCall - (await getTokenCount())).toString();
+    setTotalSupply(totalSupplyfromCall);
+    setSupplyRemaining(supplyRemainingfromCall);
+  }
+  useEffect(() => {
+    if(isWeb3Enabled){
       
-  //     updateUi(); /* do it this way because you cant use await in useEffect since it's not async */
-  //   }
-  // }, [isWeb3Enabled])
+      updateUi(); /* do it this way because you cant use await in useEffect since it's not async */
+    }
+  }, [isWeb3Enabled])
 
   const handleSuccess = async (tx) => {
     await tx.wait(1);
-    handleNewNotification(tx);
+    //handleNewNotification(tx);
     updateUi();
   }
 
-  const handleNewNotification = () => {
-    dispatch({
-      type: "info",
-      message: "Transaction Complete",
-      title: "Tx Notification",
-      positions: "topR",
-      icon: "bell"
-    })
-  }
+  //dispatch doesn't work because of some redux update, may try to figure out dependencies eventually, but not a priority
+  // const handleNewNotification = () => {
+  //   dispatch({
+  //     type: "info",
+  //     message: "Transaction Complete",
+  //     title: "Tx Notification",
+  //     positions: "topR",
+  //     icon: "bell"
+  //   })
+  // }
 
   return(
     <div className="p-5">
       {mintAddress ? (
-        <div>
+        <div className="buttonAndInfo">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto mintButton"
           onClick={async () => {await mint({
             onSuccess: handleSuccess, //success means successfully sent thru metamask, not successfully transacted
             onError: (error) => console.log(error),
             })
           }}
           disabled={isLoading || isFetching}
-          //he also did some shit for a loading animation but idgaf, just check his code
         >Mint NFT</button>
-            Price: FREE!!!
-        {/* Total Supply: {totalSupply}
-        Supply Remaining: {supplyRemaining} */}
+          <div className="mintInfo">
+              <div>Price: FREE!!!</div>
+              <div>Total Supply: {totalSupply}</div>
+              <div>Supply Remaining: {supplyRemaining}</div>
+          </div>
         </div>
       ) : (
         <div>No Contract Address Detected!!!!!!</div>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-//import ManualHeader from '../components/ManualHeader';
 import Header from "../components/Header";
 import MintNft from "../components/MintNft"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Buvarian Buttheads</title>
-        <meta name="description" content="The Greatest NFT Ever Created" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* header / connect button / nav bar */}
-      <Header />
-      <MintNft />
+      <div className="All">
+        <Head>
+          <title>Buvarian Buttheads</title>
+          <meta name="description" content="The Greatest NFT Ever Created" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <MintNft />
+      </div>
     </div>
 
 
